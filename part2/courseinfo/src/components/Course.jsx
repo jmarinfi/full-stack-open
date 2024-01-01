29e651a92ed596f0746e07e0a10c57bb59f572f3
Joan <jmarinfi@uoc.edu>
2023-12-31 19:22:23 -0500

    Exercise 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,6 +1,26 @@
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
+
+const Header = ({ name }) => {
+    return <h2>{name}</h2>
+}
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => {
+    return (
+        <>
+            {parts.map(part => <Part key={part.id} part={part} />)}
+        </>
+    )
+}
+
+const Total = ({ parts }) => {
+    return (
+        <p><b>Total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
+    )
+}
 
 const Course = ({ course }) => {
     return (
--- part2/courseinfo/src/components/Total.jsx
+++
@@ -1,7 +0,0 @@
-const Total = ({ parts }) => {
-    return (
-        <p><b>Total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
-    )
-}
-
-export default Total
